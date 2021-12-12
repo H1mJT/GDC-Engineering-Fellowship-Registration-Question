f5f9c598da9f65a49f868b4f628d1b4549cc27af
H1mJT <92359223+H1mJT@users.noreply.github.com>
2021-12-12 23:57:42 +0530

    Add files via upload

--- task.py
+++ task.py
@@ -3,43 +3,56 @@ import sys, operator
 
 
 def help():
-    print('''
-./task add 2 "hello world"    # Add a new item with priority 2 and text "hello world" to the list
-./task ls                     # Show incomplete priority list items sorted by priority in ascending order
-./task del INDEX              # Delete the incomplete item with the given index
-./task done INDEX             # Mark the incomplete item with the given index as complete
-./task help                   # Show usage
-./task report                 # Statistics''')
+    print('''Usage:-
+$ ./task add 2 "hello world"    # Add a new item with priority 2 and text "hello world" to the list
+$ ./task ls                     # Show incomplete priority list items sorted by priority in ascending order
+$ ./task del INDEX              # Delete the incomplete item with the given index
+$ ./task done INDEX             # Mark the incomplete item with the given index as complete
+$ ./task help                   # Show usage
+$ ./task report                 # Statistics''')
 
 def add(x,y):
     
     if 0<=x<=10:
         x=str(x)
         y=str(y)
-        f=open("task.txt", "a")
+        try:
+            f=open("task.txt", "a")
+        except:
+            fa= open("task.txt","w+")
+            fa.close()
+            f=open("task.txt","a")
         f.write(x+" "+y+"\n" )
+        print('Added task: "'+y+'" with priority',x)
         f.close()
+
     else:
         print('Please select allowed priority range 0-10')
 
 def ls():
     lines={}
-    file=open("task.txt")
+    try:
+        file=open("task.txt")
+    except:
+        print("No tasks remained.")
     i=0
     for line in file:
         pieces=line.split(" ",1)
         lines[pieces[1].strip("\n")]=pieces[0]
     d=sorted(lines.items(), key=operator.itemgetter(1))
     for word in d:    
-        print(i+1,".", d[i][0], " [",d[i][1], "]\n")
+        print(str(i+1)+". "+d[i][0]+ " ["+str(d[i][1])+"]\n")
         i=i+1
     file.close()
 def delete(z):
     try:
     
         lines={}
-        file=open("task.txt")
-        #print(z-1)
+        try:
+            file=open("task.txt")
+        except:
+            print("No tasks to delete.")
+        
         
         for line in file:
             pieces=line.split(" ",1)
@@ -54,7 +67,7 @@ def delete(z):
                 wor=wor[1]
                 if wor.strip("\n") != d[z-1][0]:
                     f.write(word)
-        
+        print("Deleted task #"+str(z))
     except:
         print('item with index ', z, ' does not exist. Nothing deleted.')
     
@@ -65,7 +78,12 @@ def done(m):
         
         lines={}
         file=open("task.txt")
-        fs=open("completed.txt", "a")
+        try:
+            fs=open("completed.txt", "a")
+        except:
+            fg=open("completed.txt", "w+")
+            fg.close()
+            fs=open("completed.txt", "a")
         
         #print(m-1)
         
@@ -86,7 +104,7 @@ def done(m):
                 else:
                     
                     fs.write(wor)
-        
+        print("Marked item as done.")
             
     except:
         print('no incomplete item with index', m, 'exists.')
