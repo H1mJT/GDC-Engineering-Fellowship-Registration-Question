f9d218e75e305523ff54625de84f5e7a907adf4e
Himjyoti Thakuria <92359223+H1mJT@users.noreply.github.com>
2021-12-24 14:01:04 +0530

    Add files via upload

--- task.bat
+++ task.bat
@@ -4,6 +4,8 @@
 SET func=%1
 SET prio=%2
 SET text=%3
+SET prio1="%2"
+SET text1="%3"
 shift
 
 IF %func%x == x (
@@ -12,17 +14,20 @@ IF %func%x == x (
 IF %func%x == helpx (
 	python -c "import task; task.help()")
 	
-IF %func%x == addx (
-	python -c "import task; task.add(%prio%,"%text%")")
-
+IF %func%x == addx ( IF %prio%x == x (echo Not Enough parameters in command. ) else ( IF %text%x == x (echo Not Enough parameters in command.) else ( python -c "import task; task.add("%prio1%","%text1%")")))
+	
+		
 IF %func%x == lsx (
 	python -c "import task; task.ls()")
 
 IF %func%x == reportx (
 	python -c "import task; task.report()")
 
-IF %func%x == delx (
-	python -c "import task; task.delete(%prio%)")
+IF %func%x == delx ( IF %prio%x == x (echo Not Enough parameters in command. ) else ( python -c "import task; task.delete("%prio1%")"))
+	
 
-IF %func%x == donex (
-	python -c "import task; task.done(%prio%)")
+IF %func%x == donex ( IF %prio%x == x (echo Not Enough parameters in command. ) else ( python -c "import task; task.done("%prio1%")"))
+
+IF NOT %func%x == x ( IF NOT %func%x == helpx ( IF NOT %func%x == addx ( IF NOT %func%x == lsx ( IF NOT %func%x == reportx ( IF NOT %func%x == delx ( IF NOT %func%x == donex ( 
+	echo Wrong Command. Please refer to help.
+	python -c "import task; task.help()")))))))
--- task.py
+++ task.py
@@ -3,31 +3,35 @@ import sys, operator
 
 
 def help():
-    print('''Usage:-
-$ ./task add 2 "hello world"    # Add a new item with priority 2 and text "hello world" to the list
-$ ./task ls                     # Show incomplete priority list items sorted by priority in ascending order
-$ ./task del INDEX              # Delete the incomplete item with the given index
-$ ./task done INDEX             # Mark the incomplete item with the given index as complete
-$ ./task help                   # Show usage
-$ ./task report                 # Statistics''')
+    print('''Usage :-
+$ ./task add 2 hello world    # Add a new item with priority 2 and text "hello world" to the list
+$ ./task ls                   # Show incomplete priority list items sorted by priority in ascending order
+$ ./task del INDEX            # Delete the incomplete item with the given index
+$ ./task done INDEX           # Mark the incomplete item with the given index as complete
+$ ./task help                 # Show usage
+$ ./task report               # Statistics''')
 
 def add(x,y):
     
-    if 0<=x<=10:
-        x=str(x)
-        y=str(y)
-        try:
-            f=open("task.txt", "a")
-        except:
-            fa= open("task.txt","w+")
-            fa.close()
-            f=open("task.txt","a")
-        f.write(x+" "+y+"\n" )
-        print('Added task: "'+y+'" with priority',x)
-        f.close()
 
-    else:
-        print('Please select allowed priority range 0-10')
+    try :
+        z=int(x)
+    except:
+        print("Priority must be an integer.")
+        exit()
+
+    x=str(x)
+    y=str(y)
+    try:
+        f=open("task.txt", "a")
+    except:
+        fa= open("task.txt","w+")
+        fa.close()
+        f=open("task.txt","a")
+    f.write(x+" "+y+"\n" )
+    print('Added task: "'+y+'" with priority',x)
+    f.close()
+
 
 def ls():
     lines={}
@@ -43,8 +47,19 @@ def ls():
     for word in d:    
         print(str(i+1)+". "+d[i][0]+ " ["+str(d[i][1])+"]\n")
         i=i+1
+    if i==0:
+        print("No tasks remained.")
     file.close()
+
+
+
 def delete(z):
+    try:
+        z=int(z)
+    except:
+        print("Index must be an integer.")
+        exit()
+
     try:
     
         lines={}
@@ -72,8 +87,13 @@ def delete(z):
         print('item with index ', z, ' does not exist. Nothing deleted.')
     
 
+
 def done(m):
-    
+    try:
+        m=int(m)
+    except:
+        print("Index must be an integer.")
+        exit()
     try:
         
         lines={}
@@ -124,7 +144,8 @@ def report():
     for i in CoList:
         if i:
             Counter += 1
-    print('Pending : ', Counter)
+    print('Pending :', Counter)
+    
     lines={}
     file=open("task.txt")
     i=0
@@ -133,7 +154,7 @@ def report():
         lines[pieces[1].strip("\n")]=pieces[0]
     d=sorted(lines.items(), key=operator.itemgetter(1))
     for word in d:    
-        print(i+1,".", d[i][0], " [",d[i][1], "]\n")
+        print(str(i+1)+". "+str(d[i][0])+" ["+ str(d[i][1])+ "]\n")
         i=i+1
         
     file = open("completed.txt","r")
@@ -146,11 +167,11 @@ def report():
     for i in CoList:
         if i:
             Counter += 1
-    print('Completed : ', Counter)
+    print('Completed :', Counter)
    
     file=open("completed.txt")
     i=1
     for line in file:
-        print(i,'. ', line)
+        print(str(i)+'. '+str(line))
         i=i+1
 
